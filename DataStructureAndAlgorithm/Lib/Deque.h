638506e46c38aae6c32d175b6b69a6e45d858ed8
Abhijith Dev S <subhash.abhijith.dev@gmail.com>
2021-04-25 17:27:14 +0530

    dqueue

--- DataStructureAndAlgorithm/Lib/Deque.h
+++ DataStructureAndAlgorithm/Lib/Deque.h
@@ -1,8 +1,14 @@
 #pragma once
-#include <functional>
 
-template <class T>
-class CircularQueue {
+/*
+ It is a combination of both queue and a stack.
+ there are two types of deque. 
+    Input restricted
+    Output restricted
+ */
+
+template<class T>
+class Deque {
 
 private:
     T* mQueue = nullptr;
@@ -10,13 +16,13 @@ private:
     int front = -1;
     int rear = -1;
 public:
-    CircularQueue() {
+    Deque() {
         mQueue = new T[size];
         front = -1;
         rear = -1;
     }
 
-    void enqueue(T data) {
+    void insertRear(T data) {
         if (front == -1 && rear == -1) {
             front = 0;
             rear = 0;
@@ -30,7 +36,37 @@ public:
         mQueue[rear] = data;
     }
 
-    T dequeue() {
+    void insertFront(T data) {
+        if (front == -1 && rear == -1) {
+            front = 0;
+            rear = 0;
+            mQueue[front] = data;
+            return;
+        }
+        if ((front - 1 + size) % size == rear) {
+            grow();
+        }
+        front = (front - 1 + size) % size;
+        mQueue[front] = data;
+    }
+
+    T removeRear() {
+        if (rear == -1) {
+            throw "Queue is empty";
+        }
+        if (front == rear) { // if there is only one element
+            auto data = mQueue[rear];
+            front = rear = -1;
+            return data;
+        }
+        else {
+            auto data = mQueue[rear];
+            rear = (rear - 1 + size) % size;
+            return data;
+        }
+    }
+
+    T removeFront() {
         if (front == -1) {
             throw "Queue is empty";
         }
@@ -51,7 +87,7 @@ public:
         if (front == -1 && rear == -1) {
             return str;
         }
-        for (auto idx = front; idx != (rear + 1) % size;  idx = ++idx % size) {
+        for (auto idx = front; idx != (rear + 1) % size; idx = ++idx % size) {
             str += to_string(mQueue[idx]);
         }
         return str;
--- DataStructureAndAlgorithmTest/QueueTest.h
+++ DataStructureAndAlgorithmTest/QueueTest.h
@@ -4,3 +4,4 @@
 #include "QueueWithLinkedListTest.h"
 #include "QueueWithDoubleLinkedListTest.h"
 #include "CircularQueueTest.h"
+#include "DequeTest.h"
